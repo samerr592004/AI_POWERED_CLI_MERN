66d1ba8d1104917976e5fed3e53efd33771b5fab
SAMEER KUMAR NAYAK <143753418+samerr592004@users.noreply.github.com>
2026-02-15 23:12:16 +0530

    Getting started with frontend.

---
+++ client/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { LoginForm } from '@/components/ui/login-from'
+
+const Page = () => {
+  return (
+    <>
+      <LoginForm/>
+    </>
+  )
+}
+
+export default Page
